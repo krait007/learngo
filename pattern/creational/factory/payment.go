62edeb28124b4e2bc6338830b89834175488dfc4
krait007 <jinjh008@gmail.com>
2019-04-30 07:30:57 +0800

    creational pattern

---
+++ pattern/creational/factory/payment.go
@@ -0,0 +1,53 @@
+package factory
+
+import (
+	"errors"
+	"fmt"
+)
+
+const ( 
+    Cash      = 1 
+	DebitCard = 2 
+	CreditCard = 3
+	
+) 
+
+///////////////////////////////////////////////////////////
+type PaymentMethod interface { 
+    Pay(amount float32) string 
+} 
+
+
+///////////////////////////////////////////////////////////
+func GetPaymentMethod(m int) (PaymentMethod, error) { 
+	switch m { 
+        case Cash: 
+            return new(CashPM), nil 
+        case DebitCard: 
+			return new(DebitCardPM), nil 
+	    case CreditCard:
+            return new(CreditCardPM), nil 
+        default: 
+        return nil, errors.New(fmt.Sprintf("Payment method %d not recognized\n", m)) 
+	} 
+} 
+
+
+///////////////////////////////////////////////////////////
+type CashPM struct{} 
+type DebitCardPM struct{} 
+ 
+func (c *CashPM) Pay(amount float32) string { 
+	return fmt.Sprintf("%0.2f paid using cash\n", amount) 
+} 
+ 
+func (c *DebitCardPM) Pay(amount float32) string { 
+	return fmt.Sprintf("%#0.2f paid using debit card\n", amount)
+} 
+
+///////////////////////////////////////////////////////////
+type CreditCardPM struct {} 
+
+func (d *CreditCardPM) Pay(amount float32) string { 
+    return fmt.Sprintf("%#0.2f paid using new credit card implementation\n", amount) 
+} 
